cf4ffbbb62305f4a12a88baf906af321568c3807
Arvind <sriram4rajipr@gmail.com>
2019-01-23 09:05:21 -0500

    Plots v1

--- EDF/phase1.py
+++ EDF/phase1.py
@@ -6,9 +6,9 @@ import matplotlib.pyplot as plt
 import datetime
 
 #Defining the date columns for ease of parsing when reading in the file.
-dateCols = ['datetime_beginning_utc', 'datetime_beginning_ept', 'datetime_ending_utc', 'datetime_ending_ept']
+dateCols1 = ['datetime_beginning_utc', 'datetime_beginning_ept', 'datetime_ending_utc', 'datetime_ending_ept']
 
-loadData = pd.read_csv("loads.csv",parse_dates = dateCols)
+loadData = pd.read_csv("loads.csv",parse_dates = dateCols1)
 
 #Renaming columns for convenience
 loadData.columns = ['bUTC' , 'bEPT' , 'eUTC' , 'eEPT', 'area', 'avgLoad']
@@ -18,3 +18,19 @@ loadData.columns = ['bUTC' , 'bEPT' , 'eUTC' , 'eEPT', 'area', 'avgLoad']
 Include ticks and change labels
 """
 loadData.groupby(['bUTC']).sum().plot()
+plt.show()
+
+dateCols2 = ['bid_datetime_beginning_utc', 'bid_datetime_beginning_ept']
+offerData = pd.read_csv("offers.csv", parse_dates = dateCols2)
+
+#List of unique units
+
+#Given there are multiple units where bid1 data is missing/the same over the time period
+#the data with the highest variability has been chosen for plotting purposes.
+
+unitChoice = offerData.groupby(['unit_code'])['bid1'].describe()['std'].idxmax()
+
+offerPlotData = offerData.groupby(['unit_code']).get_group('AgAHBQ4OBA8dODkxODIxMDI')
+
+plt.plot(offerPlotData.bid_datetime_beginning_utc,offerPlotData.bid1)
+plt.show()
