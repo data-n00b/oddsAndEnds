a9edd61d4bccf514d42db903dc0424725dc1ed52
Arvind <sriram4rajipr@gmail.com>
2019-01-23 07:37:23 -0500

    First plot

--- EDF/phase1.py
+++ EDF/phase1.py
@@ -3,4 +3,18 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import datetime
 
+#Defining the date columns for ease of parsing when reading in the file.
+dateCols = ['datetime_beginning_utc', 'datetime_beginning_ept', 'datetime_ending_utc', 'datetime_ending_ept']
+
+loadData = pd.read_csv("loads.csv",parse_dates = dateCols)
+
+#Renaming columns for convenience
+loadData.columns = ['bUTC' , 'bEPT' , 'eUTC' , 'eEPT', 'area', 'avgLoad']
+
+#V1 plot.
+"""
+Include ticks and change labels
+"""
+loadData.groupby(['bUTC']).sum().plot()
